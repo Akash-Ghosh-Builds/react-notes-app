0424b88f787d5471ab261f3a37eee47172959203
Akash Ghosh <akashg70509@gmail.com>
2026-08-03 23:56:29 +0530

    Complete Notes App with CRUD and Local storage

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,59 @@ import CardInput from './cardinput'
 import CardReadonly from './cardreadonly'
 
 function App() {
-  const [allNotes, setAllNotes] = useState([]);
+  const [allNotes, setAllNotes] = useState(() => {
+    const savedNotes = localStorage.getItem("note");
+
+    if (savedNotes) {
+      return JSON.parse(savedNotes);
+    }
+
+    return [];
+  });
+
   function addNote(newNote) {
     setAllNotes([...allNotes, newNote]);
   }
-  function removeNote (id){
-    setAllNotes(allNotes.filter((element)=>{
-      return   element.id != id;
+  function removeNote(id) {
+    setAllNotes(allNotes.filter((element) => {
+      return element.id != id;
     }))
   }
 
   function editNote(id, updatedHeading, updatedNote) {
-  setAllNotes(
-    allNotes.map((note) => {
-      if (note.id === id) {
-        return {
-          ...note,
-          heading: updatedHeading,
-          note: updatedNote,
-        };
-      }
+    setAllNotes(
+      allNotes.map((note) => {
+        if (note.id === id) {
+          return {
+            ...note,
+            heading: updatedHeading,
+            note: updatedNote,
+          };
+        }
 
-      return note;
-    })
-  );
-}
+        return note;
+      })
+    );
+  }
+
+  useEffect(() => {
+    localStorage.setItem("note", JSON.stringify(allNotes));
+  }, [allNotes])
+
   return (
     <>
       <h1 className='font-bold text-white text-3xl text-center mt-[20px]'>
         This is a Notes Website Using LocalStorage</h1>
 
       <CardInput onAddNote={addNote} />
-      
+
       {allNotes.map((note) => (
         <CardReadonly
           key={note.id}
           heading={note.heading}
           note={note.note}
-          id ={note.id}
-          onRemove = {removeNote}
+          id={note.id}
+          onRemove={removeNote}
           onEdit={editNote}
         />
       ))}
